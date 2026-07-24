72a357fa3b381adf0ae5ab21e35ff2710d755e6b
Muhammad Abdullah <letsmailabdullahcoder@gmail.com>
2026-07-24 14:26:41 +0500

    Delete backend directory

--- backend/src/server.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import app from "./app";
-
-const PORT = Number(process.env.PORT) || 3000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 FitZone Backend API running on port ${PORT}`);
-});
